03d469a7f5cf3d8c6a167cdc10bbffad7994e2e3
chrisvriend <59793342+chrisvriend@users.noreply.github.com>
2020-09-09 21:23:02 +0200

    update to python3.8
    updated to python3.8 + fixed cp error

--- mainscript_v9.sh
+++ mainscript_v9.sh
@@ -469,7 +469,7 @@ done
 echo "creating webpage of thalamic subsegmentations for visual QC"
 /neurodocker/create_webpage_thalsubs.sh ${outputdir}
 # copy reference segmentations to vol+qa
-cp /neurodocker/REFERENCE_1subj_thalQC.html REFERENCE_avg_thalQC.html ${outputdir}/vol+QA/
+cp /neurodocker/REFERENCE_1subj_thalQC.html /neurodocker/REFERENCE_avg_thalQC.html ${outputdir}/vol+QA/
 
 echo "extracting and plotting volume of thalamic subnuclei"
 sleep 2
